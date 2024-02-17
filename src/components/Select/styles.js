956f26b6b1fbcf227386156741a37069298b58a4
eurico77 <euricomagalhaes61@gmail.com>
2024-02-17 10:36:05 -0300

    Add styles for Select component

---
+++ src/components/Select/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Select = styled.select`
+  width: 100%;
+  height: 3.25rem;
+  border-radius: 0.25rem;
+  border: 2px solid #fff;
+  outline: none;
+  background: #fff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.04);
+  padding: 0 1rem;
+  font-size: 1rem;
+  transition: border-color 0.2s ease-in;
+`;
