ae0e2d30d8bda587fe62ad1c371139f2daa93a32
eurico77 <euricomagalhaes61@gmail.com>
2024-03-31 20:40:06 -0300

    Add global styles

---
+++ src/assets/styles/global.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Sora', sans-serif;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background: ${({ theme }) => theme.backgroundColor};
+    color: #333;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
