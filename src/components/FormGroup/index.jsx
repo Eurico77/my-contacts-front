a7bb8057610ea332ab4c21dd716c92d1ed6ffd39
eurico77 <euricomagalhaes61@gmail.com>
2024-04-02 23:33:23 -0300

    Add error prop to FormGroup component

--- src/components/FormGroup/index.jsx
+++ src/components/FormGroup/index.jsx
@@ -1,14 +1,20 @@
 import propTypes from 'prop-types';
 import { FormGroupContainer } from './styles';
 
-export function FormGroup({ children }) {
+export function FormGroup({ children, error }) {
   return (
     <FormGroupContainer>
       {children}
+      {error && <small>{error}</small>}
     </FormGroupContainer>
   );
 }
 
 FormGroup.propTypes = {
   children: propTypes.node.isRequired,
+  error: propTypes.string,
+};
+
+FormGroup.defaultProps = {
+  error: null,
 };
