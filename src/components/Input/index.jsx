1b1270ea91471cf251b2bb7849c263203f11fca7
eurico77 <euricomagalhaes61@gmail.com>
2024-04-02 23:33:29 -0300

    Refactor Input component to include error handling and additional props

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,23 @@
 import propTypes from 'prop-types';
 import { InputText } from './styles';
 
-export function Input({ placeholder }) {
-  return <InputText placeholder={placeholder} />;
+export function Input({ placeholder, error, ...props }) {
+  return (
+    <InputText
+      placeholder={placeholder}
+      $error={error}
+      {...props}
+    />
+  );
 }
 
 Input.propTypes = {
   placeholder: propTypes.string.isRequired,
+  error: propTypes.bool,
+  onChange: propTypes.func,
+  value: propTypes.string,
+};
+
+Input.defaultProps = {
+  error: false,
 };
