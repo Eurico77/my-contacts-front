a508b87492c3f7edfbe00b8748a0524fb90f1ac0
eurico77 <euricomagalhaes61@gmail.com>
2024-04-02 23:33:36 -0300

    Add Loader component

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,11 @@
+import ReactDom from 'react-dom';
+import { Overlay } from './styles';
+
+export function Loader() {
+  return ReactDom.createPortal(
+    <Overlay>
+      <div className="loader" />
+    </Overlay>,
+    document.getElementById('loader-root'),
+  );
+}
